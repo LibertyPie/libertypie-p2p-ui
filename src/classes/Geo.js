952eb10ee7bbc6c6c15e6cc485d2a2d6054ac0b0
Zak Pie <zakpie@protonmail.com>
2021-01-16 15:18:18 +0000

    Geo class & HTTP class

---
+++ src/classes/Geo.js
@@ -0,0 +1,31 @@
+/**
+ * LibertyPie (https://libertypie.com)
+ * @author LibertyPie <hello@libertypie.com>
+ * @license MIT
+ */
+
+import Cache from './Cache'
+
+export default class Geo {
+
+    /**
+     * getCountry
+     */
+    async getCountry(){
+
+
+    }
+
+    /**
+     * cloudflareStrategy
+     */
+    async getCountryByCloudflare(){
+        try {
+            
+        } catch (e) {
+            console.error("")
+        }
+    }
+
+}
+    
