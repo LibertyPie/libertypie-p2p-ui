4ea138bf3a925bdf14607a2c6cd5db8bb28ba213
Zak Pie <zakpie@protonmail.com>
2021-01-24 04:03:53 +0000

    Create chainlink.js

---
+++ src/config/chainlink.js
@@ -0,0 +1,11 @@
+/**
+ * LibertyPie (https://libertypie.com)
+ * @author LibertyPie <hello@libertypie.com>
+ * @license MIT
+ * chainlink contracts addresses
+ */
+export default {
+    kovan: {
+        "btc_usd": "0xbF7A18ea5DE0501f7559144e702b29c55b055CcB"
+    }
+}
