5b1741598e165b29f817515f3ab3cf22fa51decf
Zak Pie <zakpie@protonmail.com>
2021-01-28 02:46:53 +0000

    Update CurrencyCore.js

--- src/classes/CurrencyCore.js
+++ src/classes/CurrencyCore.js
@@ -9,7 +9,10 @@ import Status from './Status';
 import Logger from './Logger';
 import Http from './Http';
 import appConfig from "../config/app"
+
 export default class CurrencyCore {
+
+    static countryCurrencyMap = null;
 
     /**
      * get currency rate
@@ -17,6 +20,12 @@ export default class CurrencyCore {
      */
     static async getRate(to){
         try {
+
+            let cacheKey = `_currency_rate_${to}`
+
+            let cachedData = Cache.get(cacheKey)
+
+            if(cachedData != null) return Status.successPromise(null, cachedData)
 
             let resultStatus = await Http.getJson(appConfig.currency_api,{
                 symbols: to,
@@ -54,6 +63,42 @@ export default class CurrencyCore {
             Logger.error("CurrencyCore::convertCurrency ",e)
             return Status.errorPromise(window._vue("currency_converter_error",[e]))
         }
+    }
+
+    /**
+     * convertByCountryCode
+     */
+    static async convertByCountry(countryCode, amount){
+
+        let countryCodeAlpha = countryCode.toUpperCase()
+
+        return new Promise((resolve,reject)=>{
+
+            if(this.countryCurrencyMap != null){
+                resolve(this.convertCurrency(this.countryCurrencyMap[countryCodeAlpha], amount))
+                return;
+            }
+
+            //lets fetch the data 
+            import("../data/currencies.js").then(async (result)=>{
+
+                this.countryCurrencyMap = result.default
+                resolve(this.convertCurrency(this.countryCurrencyMap[countryCodeAlpha], amount))
+
+            },(error) => {
+                resolve(Status.errorPromise(window._vue("currency_converter_error",[error])))
+                Logger.error("rateByCountry: ",error)
+            })
+        })
+    }
+
+
+    /**
+     * get currency rate by country code
+     * @param {String} countryCode 
+     */
+    static async getRateByCountry(countryCode){
+        return this.convertByCountry(countryCode,1)
     }
 
 }
